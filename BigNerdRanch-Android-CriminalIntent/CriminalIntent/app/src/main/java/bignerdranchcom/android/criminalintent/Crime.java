145ea733af331bf6f8ffa6fd9e195d916dae9907
Stanley H Griggs II <shg5004@gmail.com>
2016-05-18 16:54:41 -0400

    Listing 7.5 Generated getters and setter (Crime.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/Crime.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/Crime.java
@@ -11,7 +11,19 @@ public class Crime {
     private String mTitle;
 
     public Crime() {
-        //Generate unique identifer
+        //Generate unique identifier
         mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
     }
 }
