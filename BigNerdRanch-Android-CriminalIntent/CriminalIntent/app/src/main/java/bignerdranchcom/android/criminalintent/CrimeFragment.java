9438db414572cc7a66d2ff4de0412934a844b6f1
Stanley H Griggs II <shg5004@gmail.com>
2016-05-18 17:50:44 -0400

    Listing 7.11 Overriding Fragment.onCreate(Bundle) (CrimeFragment.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
@@ -1,6 +1,14 @@
 package bignerdranchcom.android.criminalintent;
 
 import android.app.Fragment;
+import android.os.Bundle;
 
 public class CrimeFragment extends android.support.v4.app.Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
 }
