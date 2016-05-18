02550a4390275924a13a4010143204a8117f76d3
Stanley H Griggs II <shg5004@gmail.com>
2016-05-18 18:05:19 -0400

    Listing 7.12 Overriding onCreateView(…) (CrimeFragment.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package bignerdranchcom.android.criminalintent;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeFragment extends android.support.v4.app.Fragment {
     private Crime mCrime;
@@ -10,5 +13,12 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
     }
 }
