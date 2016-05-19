b80a7d2fc2ee714e9bc2d08ce5a3c437108a3bc2
Stanley H Griggs II <shg5004@gmail.com>
2016-05-19 11:49:37 -0400

    Listing 8.1 Adding more fields to Crime (Crime.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/Crime.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package bignerdranchcom.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,14 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
 
     public Crime() {
         //Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
