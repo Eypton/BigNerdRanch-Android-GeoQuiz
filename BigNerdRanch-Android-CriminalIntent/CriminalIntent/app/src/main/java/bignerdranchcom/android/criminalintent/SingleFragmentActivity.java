4fa0088ca03966bab9a6e1da55eb3b621d2289d2
Stanley H Griggs II <shg5004@gmail.com>
2016-05-25 15:09:12 -0400

    Listing 9.7 Creating an abstract Activity (SingleFragmentActivity.java)

---
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package bignerdranchcom.android.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Stan on 5/25/16.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+}
