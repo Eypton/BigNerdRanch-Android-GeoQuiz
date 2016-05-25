1860c6d98001af5d51cba03a1f82c39c552b625d
Stanley H Griggs II <shg5004@gmail.com>
2016-05-25 16:37:38 -0400

    Listing 9.11 Implement CrimeListFragment (CrimeListFragment.java)

---
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package bignerdranchcom.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Stan on 5/25/16.
+ */
+public class CrimeListFragment extends Fragment {
+
+    // Nothing yet
+    
+}
