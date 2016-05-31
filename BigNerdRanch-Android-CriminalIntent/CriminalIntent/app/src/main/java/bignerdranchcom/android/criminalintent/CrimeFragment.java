80b32a1e02fe4ed3c010205e6343dbf725039f5b
Stanley H Griggs II <shg5004@gmail.com>
2016-05-31 16:59:01 -0400

    Listing 10.4 Retrieving the extra and fetching the Crime (CrimeFragment.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeFragment.java
@@ -12,6 +12,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.UUID;
+
 public class CrimeFragment extends android.support.v4.app.Fragment {
     private Crime mCrime;
     private EditText mTitleField;
@@ -21,7 +23,9 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID) getActivity().getIntent()
+                .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
