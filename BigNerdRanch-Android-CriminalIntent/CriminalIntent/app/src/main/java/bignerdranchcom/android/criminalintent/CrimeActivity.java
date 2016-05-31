ab3159856d0b371c0ee9dd3b4100b4670999b86d
Stanley H Griggs II <shg5004@gmail.com>
2016-05-31 16:11:40 -0400

    Listing 10.2 Creating a newIntent method (CrimeActivity.java)

--- BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeActivity.java
+++ BigNerdRanch-Android-CriminalIntent/CriminalIntent/app/src/main/java/bignerdranchcom/android/criminalintent/CrimeActivity.java
@@ -1,11 +1,23 @@
 package bignerdranchcom.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 /**
  * Created by Stan on 5/25/16.
  */
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID =
+            "com.bignerdranch.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
